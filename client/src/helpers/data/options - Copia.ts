0ed18dca728d4219f1ccc3412163867dccebfb09
Camilo de Assis <camilo.kotecki10@gmail.com>
2022-12-08 15:04:33 -0300

    update

--- client/src/helpers/data/options - Copia.ts
+++
@@ -1,9 +0,0 @@
-import { AiOutlineMenu, AiOutlineHome, AiOutlineUser, AiOutlineSetting,AiOutlineLogout } from 'react-icons/ai'
-
-export  const Options = [
-    {name: "Home", link: "/", icon: AiOutlineHome},
-    {name: "User", link: "/user", icon: AiOutlineUser},
-    {name: "Settings", link: "/settings", icon: AiOutlineSetting},
-    {name: "Logout", link: "/logout", icon: AiOutlineLogout},
-   
-]
